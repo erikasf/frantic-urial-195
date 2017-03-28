c0ef1ec7059d3b35f16fa7714ad4520d85203868
Erika <erika@mendux.com>
2017-03-28 16:30:00 -0700

    hjere we go again

---
+++ macos/install.sh
@@ -0,0 +1,9 @@
+# this was retyped from the git repository of holman
+
+The Brewfile handles Homebrew-based app and library installs, but there may
+# still be updates and installables in the Mac App Store. There's a nifty
+# command line interface to it that we can use to just install everything, so
+# yeah, let's do that.
+
+echo "› sudo softwareupdate -i -a"
+sudo softwareupdate -i -a
